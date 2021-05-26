4e44f446d8d9a7f4606cb6e57d0a9321b25830b1
z <imallwitz8@gmail.com>
2021-05-26 13:22:27 -0500

    nav bar start

---
+++ posts.js
@@ -0,0 +1,6 @@
+console.log("hi");
+
+// make a database of art posts to generate/fill based on what page you're on
+
+// maybe posts from recent dates or priority numbers fill into home ? or with certain ids? unsure
+
