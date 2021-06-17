3cc511138a0be3d9a3e3e096059eb18af828293a
z <imallwitz8@gmail.com>
2021-06-17 00:15:08 -0500

    added sky high to json

--- posts.js
+++ posts.js
@@ -3,90 +3,6 @@
 // make a database of art posts to generate/fill based on what page you're on
 
 // maybe posts from recent dates or priority numbers fill into home ? or with certain ids? unsure
-const starless_images = [
-    'assets/ttrpgs/starless/rileyrefsheet1.PNG',
-    'assets/ttrpgs/starless/riley_drowning.jpg',
-    'assets/ttrpgs/starless/riley_meditate.jpg',
-    'assets/ttrpgs/starless/rileyblood.PNG',
-    'assets/ttrpgs/starless/rileyleahclose.JPG',
-    'assets/ttrpgs/starless/rileyleahliedown.PNG',
-    'assets/ttrpgs/starless/rileyleahpride1.PNG',
-    'assets/ttrpgs/starless/rileysword.jpg'
-]
-
-const dreamscape_images = [
-    'assets/ttrpgs/dreamscape/ashter.png',
-    'assets/ttrpgs/dreamscape/walkingthrustatic.JPG',
-    'assets/ttrpgs/dreamscape/asterbathingsuit.PNG',
-    'assets/ttrpgs/dreamscape/asheconcept.PNG',
-    'assets/ttrpgs/dreamscape/asterandieamy.PNG',
-    'assets/ttrpgs/dreamscape/asterandiefancy.PNG',
-    'assets/ttrpgs/dreamscape/asterandielittle.PNG',
-    'assets/ttrpgs/dreamscape/asterandieold.PNG',
-    'assets/ttrpgs/dreamscape/asterandietimeline.PNG',
-    'assets/ttrpgs/dreamscape/asterashechibialt.PNG',
-    'assets/ttrpgs/dreamscape/asterashecuddle.PNG',
-    'assets/ttrpgs/dreamscape/asterashedoodles.jpg',
-    'assets/ttrpgs/dreamscape/asterashehaircut.PNG',
-    'assets/ttrpgs/dreamscape/asterashepiggyback.PNG',
-    'assets/ttrpgs/dreamscape/asterashesweaters.PNG',
-    'assets/ttrpgs/dreamscape/asterashewedding.PNG',
-    'assets/ttrpgs/dreamscape/asterashewinter.PNG',
-    'assets/ttrpgs/dreamscape/asterashexmas.PNG',
-    'assets/ttrpgs/dreamscape/asterdad.JPG',
-    'assets/ttrpgs/dreamscape/asterdoodles.PNG',
-    'assets/ttrpgs/dreamscape/asterfashion.PNG',
-    'assets/ttrpgs/dreamscape/asteroutfits.PNG',
-    'assets/ttrpgs/dreamscape/chungfam.PNG',
-    'assets/ttrpgs/dreamscape/core3xmas.PNG',
-    'assets/ttrpgs/dreamscape/gang.JPG',
-    'assets/ttrpgs/dreamscape/gotyouflowers.JPG',
-    'assets/ttrpgs/dreamscape/sleepyheads.png'
-]
-
-const sky_high_images = [
-    'assets/ttrpgs/sky_high/refsheet_taika.PNG',
-    'assets/ttrpgs/sky_high/taikaBABY.png',
-    'assets/ttrpgs/sky_high/taikasmallfinished.png',
-    'assets/ttrpgs/sky_high/skitchin_large.jfif',
-    'assets/ttrpgs/sky_high/nguyenfamilyphoto.png',
-    'assets/ttrpgs/sky_high/itsagoodthingyourecute.png',
-    'assets/ttrpgs/sky_high/taikarhea_table.png',
-    'assets/ttrpgs/sky_high/fabian.png',
-    'assets/ttrpgs/sky_high/taika_smiley.png',
-    'assets/ttrpgs/sky_high/theyreherosandtheyreholdinghands.png',
-    'assets/ttrpgs/sky_high/NguyenFamilyBread.PNG',
-    'assets/ttrpgs/sky_high/NowThat.PNG',
-    'assets/ttrpgs/sky_high/fabian.png',
-    'assets/ttrpgs/sky_high/babyfabian.png',
-    'assets/ttrpgs/sky_high/makingbread.png',
-    'assets/ttrpgs/sky_high/oldteamteens.png',
-    'assets/ttrpgs/sky_high/oldteamadults.png',
-    'assets/ttrpgs/sky_high/taikadance.png',
-    'assets/ttrpgs/sky_high/taikaearlyconcept.png',
-    'assets/ttrpgs/sky_high/taikaearlymagic.png',
-    'assets/ttrpgs/sky_high/taikaearlypurple.png',
-    'assets/ttrpgs/sky_high/taikaknight.png',
-    'assets/ttrpgs/sky_high/taikarheaoutfits.png',
-    'assets/ttrpgs/sky_high/taikaspread.png',
-    'assets/ttrpgs/sky_high/team.png',
-    'assets/ttrpgs/sky_high/tessearlyconcept.png',
-    'assets/ttrpgs/sky_high/tessisasimp.png',
-    'assets/ttrpgs/sky_high/tesstaikahug.png',
-    'assets/ttrpgs/sky_high/taikaoutfits.png',
-    'assets/ttrpgs/sky_high/taikarheaearlyrivals.png'
-]
-
-const sky_high_animations = [
-    'assets/ttrpgs/sky_high/IdleAnimationTaikaZoom.gif',
-    'assets/ttrpgs/sky_high/cuties.gif',
-    'assets/ttrpgs/sky_high/IdleAnimationTaika.gif',
-    'assets/ttrpgs/sky_high/IdleAnimationEzra.gif',
-    'assets/ttrpgs/sky_high/IdleAnimationLilli.gif',
-    'assets/ttrpgs/sky_high/IdleAnimationLuxin.gif',
-    'assets/ttrpgs/sky_high/fabiananimation.gif',
-    'assets/ttrpgs/sky_high/taikaearlygif.gif'
-]
 
 const world_tour_images = [
     'assets/ttrpgs/world_tour/cassgetstorest.png',
@@ -142,19 +58,6 @@ const initImages = (image_vec, campaign) => {
     }
 }
 
-const initGifs = (gif_vec, campaign) => {
-    let idx = 0;
-    for (const image of gif_vec) {
-        document.getElementById(campaign).querySelector('.gifs').innerHTML += `
-            <div class="card">
-                <div class="image" 
-                    style="background-image:url('${image}')"
-                    data-index=${idx}"></div>
-            </div>`;
-        idx += 1;
-    }
-}
-
 const setFeatured = (ev) => {
     let elem = ev.currentTarget;
     let campaign = elem.parentNode.parentNode.parentNode.id;
@@ -173,6 +76,11 @@ const showFeatured = (imagedata, campaign) => {
 
 const showImages = (data, campaign) => {
     let image_vec = data[campaign];
+    image_vec.sort((a, b) => {
+        if (a.priority == 1) {
+            return -1;}
+        else {return b.date - a.date; }
+    })
     let idx = 0;
     showFeatured(image_vec[0], campaign);
     for (const image of image_vec) {
@@ -195,6 +103,7 @@ $(document).ready(function() {
         artdata = data;
         showImages(artdata, "starless");
         showImages(artdata, "dreamscape");
+        showImages(artdata, "sky-high")
         attachEventHandlers();
         });
 })
@@ -206,10 +115,8 @@ const attachEventHandlers = () => {
 };
 
 
-initImages(sky_high_images, "sky-high");
 // initImages(starless_images, "starless");
 initImages(world_tour_images, "world-tour");
 initImages(phoenix_images, "phoenix");
 initImages(children_images, "children");
 initImages(misc_images, "oneshots");
-initGifs(sky_high_animations, "sky-high");
