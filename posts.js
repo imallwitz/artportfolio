75e77cf5e7fb99485076bd03e452aedb5fc1b61f
z <imallwitz8@gmail.com>
2021-06-29 17:42:47 -0500

    slideshow on home page, star background

--- posts.js
+++ posts.js
@@ -1,7 +1,4 @@
 
-
-// make a database of art posts to generate/fill based on what page you're on
-
 // maybe posts from recent dates or priority numbers fill into home ? or with certain ids? unsure
 
 var artdata = [];
