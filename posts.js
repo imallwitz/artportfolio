4266428d8b1fca02dbb5e6f776e4b7cbdc5750f8
imallwitz <54367414+imallwitz@users.noreply.github.com>
2022-09-01 14:46:13 -0400

    added showcase buttons to campaign galleries

--- posts.js
+++ posts.js
@@ -2,11 +2,18 @@
 // maybe posts from recent dates or priority numbers fill into home ? or with certain ids? unsure
 
 var artdata = [];
+var totalindices = [];
+var currindices = [];
 
-const showFeatured = (imagedata, campaign) => {
+const showFeatured = (campaign, index) => {
+    let imagedata = artdata[campaign][index];
     document.getElementById(campaign).querySelector('.featured').innerHTML = `
-            <img class="featuredimage"
+            <div class="featuredcard">
+                <i class="fas fa-arrow-circle-left last-button" onclick="showLast(this)"></i>
+                <img class="featuredimage"
                 src='${imagedata.path}'></img>
+                <i class="fas fa-arrow-circle-right next-button" onclick="showNext(this)"></i>
+            </div>
             <h3 class="title">${imagedata.name}</h3>
             <h4 class="description">${imagedata.description}</h4>`;
 }
@@ -15,8 +22,32 @@ const setFeatured = (ev) => {
     let elem = ev.currentTarget;
     let campaign = elem.parentNode.parentNode.parentNode.id;
     let index = parseInt(elem.dataset.index);
-    let imagedata = artdata[campaign][index];
-    showFeatured(imagedata, campaign);
+    currindices[campaign] = index;
+    showFeatured(campaign, index);
+}
+
+const showNext = (elem) => {
+    let campaign = elem.parentNode.parentNode.parentNode.id;
+    idx = currindices[campaign] + 1;
+    //make sure it isn't past the end
+    if (idx >= totalindices[campaign] ){
+        idx = 0;
+    }
+    currindices[campaign] = idx;
+    showFeatured(campaign, idx);
+    console.log(campaign, idx)
+}
+
+const showLast = (elem) => {
+    let campaign = elem.parentNode.parentNode.parentNode.id;
+    idx = currindices[campaign] - 1;
+    //make sure it isn't past the end
+    if (idx < 0){
+        idx = totalindices[campaign] - 1;
+    }
+    currindices[campaign] = idx;
+    showFeatured(campaign, idx);
+    console.log(campaign, idx)
 }
 
 const showImages = (data, campaign) => {
@@ -27,7 +58,10 @@ const showImages = (data, campaign) => {
         else {return b.date - a.date; }
     })
     let idx = 0;
-    showFeatured(image_vec[0], campaign);
+    // load the first image in as the featured image
+    //showFeatured(image_vec[0], campaign);
+    showFeatured(campaign, 0);
+    // load the rest of the images in to the clickable cards 
     for (const image of image_vec) {
         document.getElementById(campaign).querySelector('.cards').innerHTML += `
             <div class="card">
@@ -37,12 +71,20 @@ const showImages = (data, campaign) => {
             </div>`;
         idx += 1;
     }
+    totalindices[campaign] = idx;
+    currindices[campaign] = 0;
 }
 
 const attachEventHandlers = () => {
     for (const elem of document.querySelectorAll('.image')) {
         elem.onclick = setFeatured;
     }
+    // for (const elem of document.querySelectorAll('.next-button')) {
+    //     elem.onclick = showNext;
+    // }
+    // for (const elem of document.querySelectorAll('.last-button')) {
+    //     elem.onclick = showLast;
+    // }
 };
 
 $(document).ready(function() {
